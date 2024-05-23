02b8a6bd4a132b0a1a299339cb8367ffe453ab67
Theodore Ross <theodoreross18@gmail.com>
2024-05-23 09:43:41 +0800

    Database Configurations

--- App/app.py
+++ App/app.py
@@ -7,3 +7,8 @@ from flask_sqlalchemy import SQLAlchemy # Database Interactions
 
 # Initializing: Flask App
 app = Flask(__name__)
+
+# Configure: Database URI (Settings)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'mysql+pymysql://flask_user:flask_password@localhost/flask_db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'supersecretkey'  # Required for flash messages
