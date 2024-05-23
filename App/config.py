daf7db33d44907777de31e22faeeec0ebbe514b1
Theodore Ross <theodoreross18@gmail.com>
2024-05-23 10:15:58 +0800

    Importing Necessary Modules

--- App/config.py
+++ App/config.py
@@ -1,1 +1,7 @@
 # Configurations: Application
+
+# Importing: Modules
+from flask import Flask # Instance of a Flask web application
+from flask_restful import Api # Create RESTful APIs in Flask
+from flask_sqlalchemy import SQLAlchemy # Database Interactions
+
