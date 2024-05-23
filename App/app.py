b2f0e828cc4fd32c82863e3e814f1667cde8c364
Theodore Ross <theodoreross18@gmail.com>
2024-05-23 21:42:41 +0800

    Error Handling on Web App

--- App/app.py
+++ App/app.py
@@ -101,3 +101,12 @@ def delete_user(id):
 # Register: Blueprint
 app.register_blueprint(main)
 
+# Error Handlers: 404, 500
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
